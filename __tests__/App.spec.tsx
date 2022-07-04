b8df8d842b5f8fc167eaa6cf011c3405ffe30026
alexandrepaulinoss <alexandrepaulinoss@gmail.com>
2022-07-04 02:14:35 -0300

    Fix test

--- __tests__/App.spec.tsx
+++ __tests__/App.spec.tsx
@@ -1,14 +1,17 @@
 import 'react-native';
 import * as React from 'react';
+import {render} from '@testing-library/react-native';
+
 import App from '../App';
 
-import {render, screen} from '@testing-library/react-native';
+jest.mock('../src/contexts/GeneralContext', () => ({
+  GeneralProvider: () => 'GeneralProvider',
+}));
 
 jest.mock('../src/screens/home', () => 'Home');
 
 describe('App is loaded', () => {
   it('renders correctly', () => {
-    const tree = render(<App />).toJSON();
-    expect(tree).toMatchObject({type: 'Home', props: {}, children: null});
+    expect(render(<App />).toJSON()).toMatch('GeneralProvider');
   });
 });
